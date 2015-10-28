08818bd94f5a88fd3b87727112d60d1e098edadf
Andrei Visan <andreivisan@andreis-mbp.home>
2015-10-28 22:32:10 +0100

    First commit

---
+++ app/src/main/java/io/programminglife/bluetoothprinter/driver/ESCPOSDriver.java
@@ -0,0 +1,21 @@
+package io.programminglife.bluetoothprinter.driver;
+
+/**
+ * Created by andreivisan on 10/28/15.
+ */
+public class ESCPOSDriver {
+
+    private static String tag = ESCPOSDriver.class.getSimpleName();
+
+    private static final byte[] LINE_FEED = {0x0A};
+    private static final byte[] PAPER_FEED = {27, 0x4A, (byte)0xFF};
+    private static final byte[] PAPER_CUT = {0x1D, 0x56, 0x1};
+    private static final byte[] ALIGN_LEFT = {0x1B, 0x61, 0};
+    private static final byte[] ALIGN_CENTER = {0x1B, 0x61, 1};
+    private static final byte[] ALIGN_RIGHT = {0x1B, 0x61, 2};
+    private static final byte[] BOLD_ON = {0x1B, 0x45, 1};
+    private static final byte[] BOLD_OFF = {0x1B, 0x45, 0};
+    private static final byte[] INIT = {0x1B, 0x40};
+    private static final byte[] STANDARD_MODE = {0x1B, 0x53};
+
+}
