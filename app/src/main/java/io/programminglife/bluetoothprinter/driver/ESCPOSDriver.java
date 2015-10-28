cf83b6defe9aefcd79e83c18d3618f1ebca400cf
Andrei Visan <andreivisan@andreis-mbp.home>
2015-10-28 22:49:21 +0100

    Added ESCPOSDriver

--- app/src/main/java/io/programminglife/bluetoothprinter/driver/ESCPOSDriver.java
+++ app/src/main/java/io/programminglife/bluetoothprinter/driver/ESCPOSDriver.java
@@ -1,5 +1,10 @@
 package io.programminglife.bluetoothprinter.driver;
 
+import android.util.Log;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
 /**
  * Created by andreivisan on 10/28/15.
  */
@@ -17,5 +22,53 @@ public class ESCPOSDriver {
     private static final byte[] BOLD_OFF = {0x1B, 0x45, 0};
     private static final byte[] INIT = {0x1B, 0x40};
     private static final byte[] STANDARD_MODE = {0x1B, 0x53};
+
+    public void initPrint(BufferedOutputStream bufferedOutputStream) {
+        try {
+            bufferedOutputStream.write(INIT);
+            bufferedOutputStream.write(STANDARD_MODE);
+        } catch (IOException e) {
+            Log.e(tag, e.getMessage(), e);
+        }
+    }
+
+    public void printLineAlignLeft(BufferedOutputStream bufferedOutputStream, String lineData) {
+        try {
+            bufferedOutputStream.write(ALIGN_LEFT);
+            bufferedOutputStream.write(lineData.getBytes());
+            bufferedOutputStream.write(LINE_FEED);
+        } catch (IOException e) {
+            Log.e(tag, e.getMessage(), e);
+        }
+    }
+
+    public void printLineAlignCenter(BufferedOutputStream bufferedOutputStream, String lineData) {
+        try {
+            bufferedOutputStream.write(ALIGN_CENTER);
+            bufferedOutputStream.write(lineData.getBytes());
+            bufferedOutputStream.write(LINE_FEED);
+        } catch (IOException e) {
+            Log.e(tag, e.getMessage(), e);
+        }
+    }
+
+    public void printLineAlignRight(BufferedOutputStream bufferedOutputStream, String lineData) {
+        try {
+            bufferedOutputStream.write(ALIGN_RIGHT);
+            bufferedOutputStream.write(lineData.getBytes());
+            bufferedOutputStream.write(LINE_FEED);
+        } catch (IOException e) {
+            Log.e(tag, e.getMessage(), e);
+        }
+    }
+
+    public void finishPrint(BufferedOutputStream bufferedOutputStream) {
+        try {
+            bufferedOutputStream.write(PAPER_FEED);
+            bufferedOutputStream.write(PAPER_CUT);
+        } catch (IOException e) {
+            Log.e(tag, e.getMessage(), e);
+        }
+    }
 
 }
